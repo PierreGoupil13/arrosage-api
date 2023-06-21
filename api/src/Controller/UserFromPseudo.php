39e78cfc8af5504d55ff5ca4e2073f8a4b21a267
PierreGoupil13 <p.goupil356@gmail.com>
2023-06-21 16:23:41 +0200

    Route pour recup un user a partir du pseudo

---
+++ api/src/Controller/UserFromPseudo.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+
+#[AsController]
+class UserFromPseudo
+{
+    public function __invoke(Request $request, EntityManagerInterface $entityManager)
+    {   
+        $pseudo = $request->query->get('pseudo');
+        #Get the user from the pseudo
+        $user = $entityManager->getRepository(Utilisateur::class)->findOneBy(['pseudo' => $pseudo]);
+        return $user->getId();
+    }
+}
