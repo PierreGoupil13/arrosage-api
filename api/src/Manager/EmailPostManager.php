67d4b11697c34ade2c4fbf6e5f053fe72fe00ed5
PierreGoupil13 <p.goupil356@gmail.com>
2023-06-21 16:01:00 +0200

    Bon user gmail

--- api/src/Manager/EmailPostManager.php
+++ api/src/Manager/EmailPostManager.php
@@ -24,20 +24,19 @@ class EmailPostManager
     {   
         #Get the user who posted the comment
         $userFrom = $this->entityManager->getRepository(Utilisateur::class)->findOneBy(['id' => $data['utilisateur']]);
-
+        #Get the annonce linked to the comment
+        $annonce = $this->entityManager->getRepository(Annonce::class)->findOneBy(['id' => $data['annonce']]);
+        $userProp = $annonce->getUtilisateurProp()->getEmail();
         #Create the email from the user who posted the comment to the user who posted the annonce
         $email = (new Email())
-            ->from($userFrom->getEmail())
-            ->to('p.goupil356@gmail.com')
+            ->from('projetarosaje@gmail.com')
+            ->to($userProp)
             ->subject('Nouveau commentaire sur votre annonce')
             ->text('Vous avez reçu un nouveau commentaire sur votre annonce')
             ->html('<p>Vous avez reçu un nouveau commentaire sur votre annonce</p>');
 
-        
-
-
-        #Get the annonce linked to the comment
-        $annonce = $this->entityManager->getRepository(Annonce::class)->findOneBy(['id' => $data['annonce']]);
+        #get the email of the user who posted the annonce
+        $userTo = $annonce->getUtilisateurProp()->getEmail();
         #send the email
         $this->mailer->send($email);
         #From annonce, get all users who posted a comment and put them in an array
@@ -49,21 +48,25 @@ class EmailPostManager
         $usersTo = $usersTo->map(function($user) {        
             return $user->getEmail();
         });
-
+        #if user who posted the annonce is in the array, remove him
+        if(in_array($userTo, $usersTo->toArray())) {
+            $usersTo = $usersTo->filter(function($user) use ($userTo) {
+                return $user != $userTo;
+            });
+        }
         #remove doublons
         $usersTo = array_unique($usersTo->toArray());
-
         #send a mail to every users who posted a comment
         foreach($usersTo as $userTo) {
             $email = (new Email())
-                ->from($userFrom->getEmail())
+                ->from('projetarosaje@gmail.com')
                 ->to($userTo)
                 ->subject('Nouveau commentaire sur une annonce que vous avez commenté')
                 ->text('Vous avez reçu un nouveau commentaire sur une annonce que vous avez commenté')
                 ->html('<p>Vous avez reçu un nouveau commentaire sur une annonce que vous avez commenté</p>');
             $this->mailer->send($email);
         }
-        #dd($usersTo);
+
         $commentaire = new Commentaire();
         $commentaire->setAnnonce($annonce);
         $commentaire->setUtilisateur($userFrom);
