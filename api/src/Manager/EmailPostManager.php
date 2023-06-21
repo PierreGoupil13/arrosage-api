ccf29e174cadf52de2cb8a2a08d75de7ea53987c
PierreGoupil13 <p.goupil356@gmail.com>
2023-06-21 15:36:35 +0200

    Implementation de gmail

--- api/src/Manager/EmailPostManager.php
+++ api/src/Manager/EmailPostManager.php
@@ -7,13 +7,17 @@ use App\Entity\Annonce;
 use App\Entity\Commentaire;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class EmailPostManager
 {
     private EntityManagerInterface $entityManager;
-    public function __construct(EntityManagerInterface $entityManager)
+    private MailerInterface $mailer;
+    public function __construct(EntityManagerInterface $entityManager, MailerInterface $mailer)
     {
         $this->entityManager = $entityManager;
+        $this->mailer = $mailer;
     }
 
     public function getUsers(mixed $data)
@@ -21,21 +25,44 @@ class EmailPostManager
         #Get the user who posted the comment
         $userFrom = $this->entityManager->getRepository(Utilisateur::class)->findOneBy(['id' => $data['utilisateur']]);
 
+        #Create the email from the user who posted the comment to the user who posted the annonce
+        $email = (new Email())
+            ->from($userFrom->getEmail())
+            ->to('p.goupil356@gmail.com')
+            ->subject('Nouveau commentaire sur votre annonce')
+            ->text('Vous avez reçu un nouveau commentaire sur votre annonce')
+            ->html('<p>Vous avez reçu un nouveau commentaire sur votre annonce</p>');
+
+        
+
+
         #Get the annonce linked to the comment
         $annonce = $this->entityManager->getRepository(Annonce::class)->findOneBy(['id' => $data['annonce']]);
-
+        #send the email
+        $this->mailer->send($email);
         #From annonce, get all users who posted a comment and put them in an array
         $usersTo = $annonce->getCommentaires()->map(function($commentaire) {
             return $commentaire->getUtilisateur();
         });
 
         #Get the emails of every unique users who posted a comment
-        $usersTo = $usersTo->map(function($user) {
+        $usersTo = $usersTo->map(function($user) {        
             return $user->getEmail();
         });
 
         #remove doublons
         $usersTo = array_unique($usersTo->toArray());
+
+        #send a mail to every users who posted a comment
+        foreach($usersTo as $userTo) {
+            $email = (new Email())
+                ->from($userFrom->getEmail())
+                ->to($userTo)
+                ->subject('Nouveau commentaire sur une annonce que vous avez commenté')
+                ->text('Vous avez reçu un nouveau commentaire sur une annonce que vous avez commenté')
+                ->html('<p>Vous avez reçu un nouveau commentaire sur une annonce que vous avez commenté</p>');
+            $this->mailer->send($email);
+        }
         #dd($usersTo);
         $commentaire = new Commentaire();
         $commentaire->setAnnonce($annonce);
